6392ae70a7b95d48c341943e7de86cac69058b45
YoungKim <ky200223@gmail.com>
2014-04-05 22:48:48 +0900

    hot fix. 새로운 소식이 계속 뜸. -3

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -101,7 +101,7 @@ var pageFunction = (function () {
     var load = {
         checkNewCard: function () {
             var socket = io.connect('http://www.skkuleaf.com');
-            socket.on('newCard', load.showButton);
+            //socket.on('newCard', load.showButton);
             console.log('check');
         },
         showButton: function (number) {
