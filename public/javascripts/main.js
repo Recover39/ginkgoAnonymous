4a05370106dd85854da5d09253bc74f9d4da336e
YoungKim <ky200223@gmail.com>
2014-04-05 14:25:35 +0900

    delete comments at main.js

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,45 +1,9 @@
 var pageWriteFunction = (function () {
     var card = {
-        write: function (e) {
-            e.preventDefault();
-
-            var curForm = e.currentTarget.form,
-                sendFormData = new FormData(curForm),
-                appendTarget = document.getElementsByClassName('cardList')[0].childNodes[0].childNodes[0],
-                url = "/card/add",
-                request = new XMLHttpRequest();
-
-            request.open("POST", url, true);
-            request.onreadystatechange = function () {
-                if (request.readyState == 4 && request.status == 200) {
-                    var res = JSON.parse(request.responseText),
-                    // later, change String object to DOM Object
-                        card = '<div class="col-md-6 col-md-offset-3 card"><div class="cardInfomation">' +
-                            '<div class="cardNum"> No.' + res._id + '</div><div class="cardCommitTime"> 방금 </div>' +
-                            '</div><div class="cardBody">' + res.body + '</div><div class="cardStatus">' +
-                            '<div class="likeCount">은행 115개</div><div class="commitCount">댓글 0개</div></div>' +
-                            '<div class="cardComment"><div class="commentList"></div>' +
-                            '<form action="/card/' + res._id + '/comment/add" method="post" role="form" class="commentCommit">' +
-                            '<input type="hidden" name="cardId" value="' + res._id + '"/><textarea rows="1" cols="20" type="text"' +
-                            ' name="commentBody" class="commentBody"></textarea><button type="submit" ' +
-                            'class="btn btn-lg btn-block commentButton"> 댓글</button></form></div></div>';
-                    appendTarget.insertAdjacentHTML('afterbegin', card);
-                }
-            };
-            //ajax가 동작하지 않더라도 request에서 formdata를 보내준다.
-            request.send(sendFormData);
-            //ajax 통신이 끝난 후 폼의 값을 초기화 해준다.
-            //curForm[0] => textField
-            curForm[0].value = "";
-
-            comment.addEvent();
-        },
-
         addEvent: function () {
             var writeArea = document.getElementById('postTextArea'),
                 writeButton = document.getElementById('writeButton');
             writeArea.addEventListener("click", util.writeSectionExpand, true);
-            //writeButton.addEventListener("click", card.write, true);
         }
     };
 
@@ -123,37 +87,6 @@ var pageWriteFunction = (function () {
 
 var pageFunction = (function () {
     var load = {
-        cardNum: 0,
-        isFirst: true,
-//        checkNewCard: function () {
-//            $.ajax({
-//                type: "GET",
-//                url: "/card/checkNewCard",
-//                success: function (obj) {
-//                    load.showButton(obj.data.length);
-//                }
-//            });
-//        },
-        showButton: function (number) {
-            if (load.isFirst == true) {
-                load.isFirst = false;
-                load.cardNum = number;
-            }
-            else {
-                if (load.cardNum != number) {
-                    $("#newCardButton").css("display", "block");
-                }
-            }
-
-        },
-        hideButton: function () {
-            $("#newCardButton").css("display", "none");
-        },
-        newCard: function () {
-            window.location.reload();
-            load.hideButton();
-            window.location = "/card#newCardStart";
-        },
         toMain: function () {
             window.location.reload();
             window.location = "/";
@@ -175,15 +108,6 @@ var pageFunction = (function () {
 
                 //later, add color to Time
                 cardInfoDiv[i].childNodes[1].innerHTML = "삭제까지 " + elapsedHours + "시간 " + elapsedMinutes + "분 남음";
-                // if card's aging is over, auto delete item (execute at client)
-                // now execute at server
-//                if (elapsedHours <= 0 && elapsedMinutes <= 0) {
-//                    var cardNo = cardInfoDiv[i].childNodes[0].innerText,
-//                        cardNoCountPattern = /\d+/,
-//                        card_id = cardNoCountPattern.exec(cardNo);
-//
-//                    cardInfo.deleteCard(card_id);
-//                }
             }
         },
 
@@ -210,9 +134,6 @@ var pageFunction = (function () {
 (function () {
     pageWriteFunction.cardEventAdd();
     pageWriteFunction.commentEventAdd();
-
-    //refresh every 30sec
-    //setInterval(pageFunction.checkNewCard, 30000);
 })();
 
 var newCard = pageFunction.newCard;
