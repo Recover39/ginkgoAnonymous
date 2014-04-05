896bf542e92bd0c47ef07904f6ab55d8e62b953e
YoungKim <ky200223@gmail.com>
2014-04-05 20:03:05 +0900

    새로운 글 작성시 생기는 문제 해결. write 함수에서 userCardNum === 0 일 때 처리.

--- routes/index.js
+++ routes/index.js
@@ -524,6 +524,9 @@ exports.write = function (req, res) {
             else {
                 // maximum : 30
                 var userCardNum = result.length;
+                if (userCardNum === 0) {
+                    writeCard(req,res);
+                }
                 if (userCardNum === 30) {
                     var lastCardTime = curTime - result[userCardNum].date;
                     if (lastCardTime < ms6Hour) {
